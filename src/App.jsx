b42bc5c3074e3b6bd321b4b57466d03baa5a48f8
Shivantha56 <shiwanthadilshan3@gmail.com>
2025-07-11 23:15:41 +0530

    Add subtitles for the video track

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <video 
+        src="https://ik.imagekit.io/roadsidecoder/yt/example.mp4?tr=w-800" 
+        width={"800"}
+        height={"500"}
+        controls
+        // autoPlay
+        muted
+        // loop
+        poster='https://ik.imagekit.io/roadsidecoder/yt/example.mp4/ik-thumbnail.jpg?tr=so-5'
+      >
+        <track 
+          kind='subtitles'
+          src='/subtitles/english.vtt'
+          srcLang='en'
+          label='English'
+          default
+        />
+        <track 
+          kind='subtitles'
+          src='/subtitles/hindi.vtt'
+          srcLang='hi'
+          label='Hindi'
+        />
+      </video>
     </>
   )
 }
