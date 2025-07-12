f3d41a57e120d0685939746df96fd9c55561b79a
Shivantha56 <shiwanthadilshan3@gmail.com>
2025-07-12 13:11:11 +0530

    Update video js component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-import VideoJs from './components/Video'
-import './App.css'
+import { useRef } from 'react';
+import VideoJs from './components/Video';
 
 function App() {
+  const playerRef = useRef(null);
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [{
+      src: 'https://ik.imagekit.io/roadsidecoder/yt/example.mp4',
+      type: 'video/mp4'
+    }]
+
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on('waiting', () => {
+      videojs.log('player is waiting');
+    });
+
+    player.on('dispose', () => {
+      videojs.log('player will dispose');
+    });
+  };
+
   return (
-    <VideoJs />
+    <div style={{maxWidth:"800px", width:"800px"}}>  
+      <VideoJs 
+        options={videoJsOptions} 
+        onReady={handlePlayerReady} 
+      />
+    </div>
   )
 }
 
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,7 +1,51 @@
-const VideoJs = () => {
-    return <h1 style={"color:green"}>
-        Dilshan SSSS
-    </h1>;
+import { useEffect, useRef } from "react";
+import videojs from 'video.js';
+import "video.js/dist/video-js.css"
+
+const VideoJs = ({options, onReady}) => {
+    const playerRef = useRef(null);
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+         // Make sure Video.js player is only initialized once
+        if(!playerRef.current){
+            const videoElement = document.createElement("video-js");
+
+            videoElement.classList.add('vjs-big-play-centered',"video-js");
+            videoRef.current.appendChild(videoElement);
+
+            const player = playerRef.current = videojs(videoElement, options, () => {
+                videojs.log('player is ready');
+                onReady && onReady(player);
+            });
+
+        }else{
+            const player = playerRef.current;
+
+            player.autoplay(options.autoplay);
+            player.src(options.src)
+        }
+
+
+    },[options, videoRef])
+
+    // Dispose the Video.js player when the functional component unmounts
+    useEffect(() => {
+        const player = playerRef.current;
+
+        return () => {
+            if (player && !player.isDisposed()) {
+                player.dispose();
+                playerRef.current = null;
+            }
+        };
+    }, [playerRef]);
+    
+    return (
+        <div data-vjs-player>
+            <div ref={videoRef} />
+        </div>
+    );
 }
 
 export default VideoJs;
